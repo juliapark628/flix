40a40e9dce5a69815422a9a248e280bd5afd7c1a
Julia Park <juliapark628@fb.com>
2019-06-26 12:13:52 -0700

    Second initial commit

---
+++ flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flix
+//
+//  Created by juliapark628 on 6/26/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flix
+//
+//  Created by juliapark628 on 6/26/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
