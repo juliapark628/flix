d93e0ef3daf0249e0ede5de4d9a1c207849f0850
Julia Park <juliapark628@fb.com>
2019-06-27 17:45:03 -0700

    added detail view

---
+++ flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.h
+//  flix
+//
+//  Created by juliapark628 on 6/27/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie; 
+
+@end
+
+NS_ASSUME_NONNULL_END
